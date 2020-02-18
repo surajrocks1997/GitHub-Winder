2f7bfee7bba243192d1c5ba6f9b619a7f0ebb044
surajrocks1997 <surajrocks1997@gmail.com>
2020-02-18 19:15:35 +0530

    Project Completed

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Navbar = ({icon, title}) => {
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                <i className={icon} />
+                <Link to='/'>{title}</Link>
+            </h1>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+Navbar.defaultProps = {
+    title: "Github Winder",
+    icon: "fab fa-github fa-lg"
+};
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
+
+export default Navbar;
---
+++ src/components/users/User.js
@@ -0,0 +1,116 @@
+import React, { Fragment, useEffect, useContext } from "react";
+import Spinner from "../layout/Spinner";
+import Repos from "../repos/Repos";
+import { Link } from "react-router-dom";
+import GithubContext from "../../context/github/githubContext";
+
+const User = ({ match }) => {
+
+    const githubContext = useContext(GithubContext);
+    const { user, loading, getUser, getUserRepos, repos } = githubContext;
+
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        //eslint-disable-next-line
+    }, []);
+
+    const {
+        name,
+        avatar_url,
+        location,
+        bio,
+        blog,
+        login,
+        company,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable
+    } = user;
+
+    if (loading) return <Spinner />;
+
+    return (
+        <Fragment>
+            <Link to="/" className="btn btn-light">
+                <i className="fas fa-arrow-alt-circle-left fa-lg"></i>Back
+            </Link>
+            Hireable:{" "}
+            {hireable ? (
+                <i className="fas fa-check text-success" />
+            ) : (
+                <i className="fas text-danger fa-times-circle" />
+            )}
+            <div className="card grid-2">
+                <div className="all-center">
+                    <img
+                        src={avatar_url}
+                        className="round-img"
+                        style={{ width: "200px" }}
+                        alt=""
+                    />
+                    <h1>{name}</h1>
+                    <p>Location: {location}</p>
+                </div>
+                <div>
+                    {bio && (
+                        <Fragment>
+                            <h3>Bio</h3>
+                            <p>{bio}</p>
+                        </Fragment>
+                    )}
+                    <a href={html_url} className="btn btn-dark my-1">
+                        Visit GitHub Profile
+                    </a>
+                    <ul>
+                        <li>
+                            {login && (
+                                <Fragment>
+                                    <strong>Username : </strong> {login}
+                                </Fragment>
+                            )}
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            {company && (
+                                <Fragment>
+                                    <strong>Company : </strong> {company}
+                                </Fragment>
+                            )}
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            {blog && (
+                                <Fragment>
+                                    <strong>Blog : </strong> {blog}
+                                </Fragment>
+                            )}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="card text-center">
+                <div className="badge badge-primary">
+                    Followers: {followers}
+                </div>
+                <div className="badge badge-success">
+                    Following: {following}
+                </div>
+                <div className="badge badge-light">
+                    Public Repos: {public_repos}
+                </div>
+                <div className="badge badge-dark">
+                    Public Gists: {public_gists}
+                </div>
+            </div>
+            <Repos repos={repos} />
+        </Fragment>
+    );
+};
+
+export default User;
